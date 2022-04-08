0b8f50db48ffa5143ce5544f2f5ce1abe3b709cd
adam_holgersson <adam.holgersson@hotmail.se>
2022-04-08 14:26:04 +0200

    Added a function in player that instantiates a player object with default values

---
+++ game/player.py
@@ -0,0 +1,14 @@
+"""Handle most things related to the player(s)."""
+
+
+class Player():
+    """Handle the player object."""
+
+    def __init__(self):
+        """Set default values to the object."""
+        self.name = ""
+        self.score = 0
+
+
+if __name__ == '__main__':
+    player = Player()
