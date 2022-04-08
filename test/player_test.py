72d17ec3bf75eae4dbb8c3fe2e370e51e06c5f80
adam_holgersson <adam.holgersson@hotmail.se>
2022-04-08 14:19:40 +0200

    Added player class and test class for it, added test for instantiating a player object

---
+++ test/player_test.py
@@ -0,0 +1,23 @@
+"""Unittesting"""
+
+import unittest
+from game import player
+
+class TestPlayerClass(unittest.TestCase):
+    """Testing the class."""
+
+    def test_init_player_object(self):
+        """Instantiate a player object and test its base values."""
+        player1 = player.Player()
+        self.assertIsInstance(player1, player.Player)
+
+        res1 = player1.name
+        exp1 = ""
+        self.assertEqual(res1, exp1)
+
+        res2 = player1.score
+        exp2 = 0
+        self.assertEqual(res2, exp2)
+
+if __name__ == '__main__':
+    unittest.main()
