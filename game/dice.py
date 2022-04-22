1f5ce6360fa0f70256792ab4dfefdf87cc59dd8f
adam_holgersson <adam.holgersson@hotmail.se>
2022-04-22 16:00:18 +0200

    Added the function to roll a dice

--- game/dice.py
+++ game/dice.py
@@ -6,8 +6,18 @@ class Dice():
     """Handle the dice object."""
 
     faces = 6
-    
+
     def __init__(self):
         """Instantiate object."""
         self.rolls_made = 0
         random.seed()
+
+    def roll_dice(self):
+        """Roll a dice."""
+        roll = random.randint(1, self.faces)
+        self.rolls_made += 1
+
+        return roll
+
+    if __name__ == '__main__':
+        die = Dice()
