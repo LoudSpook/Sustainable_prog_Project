e360f0b6950e7cdc6ca7b968c3e884443764328b
adam_holgersson <adam.holgersson@hotmail.se>
2022-04-23 14:24:35 +0200

    Optimized test classes

--- test/dice_test.py
+++ test/dice_test.py
@@ -6,41 +6,43 @@ from game import dice
 class TestDiceClass(unittest.TestCase):
     """Test the dice class."""
 
+    def setUp(self):
+        self.dice = dice.Dice()
+
+    def tearDown(self):
+        del self.dice
+
     def test_init_dice_object(self):
         """Instantiate a dice object and test its values."""
-        die = dice.Dice()
-        self.assertIsInstance(die, dice.Dice)
+        self.assertIsInstance(self.dice, dice.Dice)
 
-        res = die.faces
+        res = self.dice.faces
         exp = 6
         self.assertEqual(res, exp)
 
     def test_roll_dice(self):
         """Roll a dice and test the roll."""
-        die = dice.Dice()
-        roll = die.roll_dice()
-        expected = 1 <= roll <= die.faces
+        roll = self.dice.roll_dice()
+        expected = 1 <= roll <= self.dice.faces
         self.assertTrue(expected)
 
     def test_get_rolls_made(self):
         """Roll dice, get the amount of rolls made and test to see
         if it's correct."""
-        die = dice.Dice()
         default_amount = 0
-        die.roll_dice()
-        roll_amount = die.get_rolls_made()
+        self.dice.roll_dice()
+        roll_amount = self.dice.get_rolls_made()
         self.assertNotEqual(default_amount, roll_amount)
 
     def test_clean_rolls_made(self):
         """Roll a dice a few times then reset the number of rolls made
         back to 0 and test if rolls_made was reset."""
-        die = dice.Dice()
         for x in range(3):
-            die.roll_dice()
+            self.dice.roll_dice()
 
-        die.clean_rolls_made()
+        self.dice.clean_rolls_made()
         exp = 0
-        self.assertEqual(exp, die.get_rolls_made())
+        self.assertEqual(exp, self.dice.get_rolls_made())
 
 if __name__ == '__main__':
     unittest.main()
--- test/player_test.py
+++ test/player_test.py
@@ -6,41 +6,45 @@ from game import player
 class TestPlayerClass(unittest.TestCase):
     """Testing the class."""
 
-    def test_init_player_object(self):
-        """Instantiate a player object and test its base values."""
-        player1 = player.Player()
-        self.assertIsInstance(player1, player.Player)
+    def setUp(self):
+        self.player = player.Player()
 
-        res1 = player1.name
+    def tearDown(self):
+        del self.player
+
+    def test_init_player_object(self):
+        """Test if a player object was instantiated correctly and
+        test its default values."""
+        self.assertIsInstance(self.player, player.Player)
+
+        res1 = self.player.name
         exp1 = ""
         self.assertEqual(res1, exp1)
 
-        res2 = player1.score
+        res2 = self.player.score
         exp2 = 0
         self.assertEqual(res2, exp2)
 
     def test_name(self):
-        """Select and changes a player name and test if the name was
+        """Select and change a player's name and test if the name was
         properly changed both times (use two different names)."""
-        player1 = player.Player()
-        name = player1.select_name()
+        name = self.player.select_name()
         err = ""
 
-        self.assertNotEqual(err, player1.name)
-        self.assertEqual(player1.name, name)
+        self.assertNotEqual(err, self.player.name)
+        self.assertEqual(self.player.name, name)
 
-        new_name = player1.change_name()
+        new_name = self.player.change_name()
         self.assertNotEqual(name, new_name)
-        self.assertEqual(player1.name, new_name)
+        self.assertEqual(self.player.name, new_name)
 
     def test_add_score(self):
-        """Adds a score of 3 to the players total and
-        tests if it is added correctly"""
-        player1 = player.Player()
-        old_score = player1.score
-        player1.add_score(3)
-        self.assertNotEqual(0, player1.score)
-        self.assertNotEqual(old_score, player1.score)
+        """Add a score of 3 to the players total and
+        test if it is added correctly."""
+        old_score = self.player.score
+        self.player.add_score(3)
+        self.assertNotEqual(0, self.player.score)
+        self.assertNotEqual(old_score, self.player.score)
 
 if __name__ == '__main__':
     unittest.main()
