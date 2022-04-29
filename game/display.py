45c2eef27c9eeaf37405b6d83920a065742d8f69
adam_holgersson <adam.holgersson@hotmail.se>
2022-04-29 19:24:02 +0200

    Added all functions in display class, added testcases for said functions

--- game/display.py
+++ game/display.py
@@ -6,3 +6,22 @@ class Display():
 
     def __init__(self):
         """Instantiate object."""
+
+    def print_start_menu(self):
+        print("-------------------------\n"
+        "1. Single player\n"
+        "2. Multiplayer\n"
+        "3. Exit\n"
+        "-------------------------")
+
+    def print_rules(self):
+        print("RULES\n"
+        "-------------------------\n"
+        "Take turns rolling the die.\n"
+        "You can roll until you roll a 1 or decide to hold.\n"
+        "If you decide to hold, you add the numbers from every roll and\n"
+        "add that to your total score.\n"
+        "If you roll a 1, you get no points from that round.\n"
+        "After you've rolled a 1 or decided to hold, it's next players turn.\n"
+        "First to a score total of 100 wins.\n"
+        "Good luck and have fun!")
--- test/display_test.py
+++ test/display_test.py
@@ -2,6 +2,7 @@
 
 import unittest
 from game import display
+from unittest import mock
 
 class testDisplayClass(unittest.TestCase):
     """Test the display class."""
@@ -15,3 +16,23 @@ class testDisplayClass(unittest.TestCase):
     def test_init_display_object(self):
         """Test if a display object is instantiated properly."""
         self.assertIsInstance(self.display, display.Display)
+
+    @mock.patch("game.display.print")
+    def test_print_start_menu(self, mock_print_start_menu):
+        self.display.print_start_menu()
+        mock_print_start_menu.assert_called_with("-------------------------\n"
+        "1. Single player\n2. Multiplayer\n3. Exit\n-------------------------")
+
+    @mock.patch("game.display.print")
+    def test_print_rules(self, mock_print_rules):
+        self.display.print_rules()
+        mock_print_rules.assert_called_with("RULES\n"
+        "-------------------------\n"
+        "Take turns rolling the die.\n"
+        "You can roll until you roll a 1 or decide to hold.\n"
+        "If you decide to hold, you add the numbers from every roll and\n"
+        "add that to your total score.\n"
+        "If you roll a 1, you get no points from that round.\n"
+        "After you've rolled a 1 or decided to hold, it's next players turn.\n"
+        "First to a score total of 100 wins.\n"
+        "Good luck and have fun!")
